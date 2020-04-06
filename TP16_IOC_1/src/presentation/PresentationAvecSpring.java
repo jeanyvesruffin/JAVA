4135f54e70f154959d8ed0eb918d72abc6648a62
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-04-06 14:31:04 +0200

    add file spring

---
+++ TP16_IOC_1/src/presentation/PresentationAvecSpring.java
@@ -0,0 +1,12 @@
+package presentation;
+
+public class PresentationAvecSpring {
+	// 1er on importe les fichier necaissaire à Spring (version manuel copier/ coller )
+	// + ajout ds le clsse path du projet
+	// 2eme creer un fichier de configuration de Spring 
+	public static void main(String[] args) {
+		// on creer le fichier de configuration spring .xml
+		
+	}
+
+}
