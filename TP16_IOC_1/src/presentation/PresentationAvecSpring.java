dcd535d4cc1cf6795e5331f38874ea795bc20f10
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-04-06 14:57:35 +0200

    add applicationContext qui definie les beans

--- TP16_IOC_1/src/presentation/PresentationAvecSpring.java
+++ TP16_IOC_1/src/presentation/PresentationAvecSpring.java
@@ -1,11 +1,29 @@
 package presentation;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import metier.IMetier;
+
 public class PresentationAvecSpring {
 	// 1er on importe les fichier necaissaire à Spring (version manuel copier/ coller )
 	// + ajout ds le clsse path du projet
 	// 2eme creer un fichier de configuration de Spring 
 	public static void main(String[] args) {
-		// on creer le fichier de configuration spring .xml
+		// on creer le fichier de configuration spring applicationContext.xml
+		// on demande a spring de creer un objet daoImpl au demarrage de l'application
+		// on injecte les dependances
+		/* 	
+			<bean id="d" class="dao.DaoImpl"></bean>
+				<bean id="m" class="metier.MetierImpl">
+					<property name="dao" ref="d"></property>
+			</bean> 
+		 */
+		// on demande de spring de lire le fichier 
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml"}); 
+		// on demande à spring ("donne moi un objet metier)
+		IMetier metier = (IMetier) applicationContext.getBean("metier");
+		System.out.println(metier.calcul());
 		
 	}
 
