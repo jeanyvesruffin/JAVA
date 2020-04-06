738084d5d52d09af9952552069f8a78ddd3a4019
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-04-06 11:10:24 +0200

    ok

---
+++ TP15_ClientJeuRMI/src/ClientJeu.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import jeu.IJeuRemote;
+
+public class ClientJeu {
+
+	public static void main(String[] args) {
+		try {
+			// on doit juste utiliser l'interface
+			Context context = new InitialContext();
+			// utilisation de l'ionterface
+			IJeuRemote stub = (IJeuRemote) context.lookup("JH");
+			// creation du scanner
+			Scanner clavier = new Scanner(System.in);
+			while(true) {
+				System.out.println("Deviner : ");
+				int nombreSaisi = clavier.nextInt();
+				if (nombreSaisi == -1) {
+					break;
+				}
+				String reponse = stub.jouer(nombreSaisi);
+				System.out.println(reponse);
+
+
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+}
