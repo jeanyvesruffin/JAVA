08ea4b78f62d6dea14ffff8519600a4cd6a62872
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-04-06 13:35:27 +0200

    avec Spring

--- TP16_IOC_1/src/presentation/Presentation.java
+++ TP16_IOC_1/src/presentation/Presentation.java
@@ -1,16 +1,18 @@
 package presentation;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 import dao.IDao;
+import metier.IMetier;
 
 //import dao.DaoImpl;
 //import metier.MetierImpl;
 
 public class Presentation {
 
-	@SuppressWarnings("deprecation")
+	@SuppressWarnings({ "deprecation", "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		// IL FAUT FAIRE DE L'INJECTION DE DEPENDANCE
 		// ===>  dit injection par instanciation static
@@ -46,6 +48,8 @@ public class Presentation {
 			// metierClassName ="metier.MetierImpl"
 			// charger la class dynamiquement en memoire
 			Class cDao = Class.forName(daoClassName);
+			// on charge toujours dynamiquement la class metier
+			Class cMetier = Class.forName(metierClassName);
 			// on instancie cDaoa à l'aide de la methode newInstance qui equivault à
 			// new DaoImpl() mais qui retourne un objet de type daoImpl
 			// mais sans utiliser la class sinon couplagefort
@@ -53,14 +57,23 @@ public class Presentation {
 			//// INSTANCIATION DYNAMIQUE
 			IDao dao = (IDao) cDao.newInstance();
 			// on test
-			System.out.println(dao.getTemperature());
+			// L'intere et que si l'on redefinie un fichier DaoImpl2
+			// alors nous aurons juste à modifier le fichier de configuration pour ca prise en charge
+			// System.out.println(dao.getTemperature());
+			// on fait maintenant l'association
+			// on instancie
+			IMetier metier = (IMetier) cMetier.newInstance();
+			// Il faut absolument creer des class avec constructeur sans parametre
+			// ASSOCIATION
+			// Class Method " reflect" parametres à separer de virgule
+			Method methode = cMetier.getMethod("setDao", new Class[] {IDao.class});
+			// on appel la methode avec invoke()
+			methode.invoke(metier, new Object[] {dao});
+			// C'est ca l'injection de dependance
+			System.out.println(metier.calcul());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-
-
 	}
-
 }
